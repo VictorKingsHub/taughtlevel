77bf9fe9c07f45dd2c70a28ee9b01973bb7830c2
VictorKingsHub <ezevictornkemjika@gmail.com>
2024-08-09 20:09:11 +0100

    ser

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/ui/nav/welcome/HeroSection";
+import Services from "@/ui/nav/welcome/Services";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main >
       <HeroSection />
+      <Services />
     </main>
   );
 }
---
+++ components/utils/utilities.ts
@@ -0,0 +1,8 @@
+
+export const ServicesItems = [
+    {label: "Software Engineering", desc: "This involves applying engineering principles to design, develop, and maintain web-based applications. It focuses on creating efficient, scalable, and user-friendly websites that meet specific business needs", href: "#"},
+    {label: "Website Development", href: "/whatwedo"},
+    {label: "Coding & Robotics", href: "/whoweare"},
+    {label: "Training Academy", href: "/training"},
+    {label: "Virtual Assistance", href: "/contact"},
+]
