aeb7aaa8390793ca95d546225d93dde4e6202fe8
VictorKingsHub <ezevictornkemjika@gmail.com>
2024-10-16 14:01:15 +0100

    button fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import HeroSection from "@/ui/nav/welcome/HeroSection";
 import Services from "@/ui/nav/welcome/Services";
+import Touch from "@/ui/nav/welcome/Touch";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <main >
       <HeroSection />
       <Services />
+      <Touch />
     </main>
   );
 }
---
+++ ui/nav/welcome/Touch.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./Touch.module.css"
+
+const Touch = () => {
+  return (
+    <div className={styles.container}>
+      Touch
+    </div>
+  )
+}
+
+export default Touch
