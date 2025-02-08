67ed43b6207d97d9527e741aa8ab11222b69b8f4
VictorKingsHub <ezevictornkemjika@gmail.com>
2025-02-09 03:05:03 +0800

    color change

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -89,7 +89,10 @@ const config = {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+	require("tailwindcss-animate"),
+	require('daisyui')
+],
 } satisfies Config
 
 export default config
